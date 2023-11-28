ec581f7cd3f375950eba9bf642a1510eee201232
Isai Cordero Garcia <isaicordero03@gmail.com>
2023-11-28 16:17:49 +0000

    dsaasf

--- python/S1_ Manejo de arrays/main.py
+++ python/S1_ Manejo de arrays/main.py
@@ -1,13 +1,13 @@
 from solve_tsp import *
 
 string1= input()
-parent1 =  [8,11,3,5,6,4,2,12,1,9,7,10]
+parent1 = [int(k) for k in string1.split(',')]
 
 string2= input()
-parent2 =  [1,2,3,4,5,6,7,8,9,10,11,12]
+parent2 = [int(k) for k in string2.split(',')]
 
-lower_bound = 6
-upper_bound = 9
+lower_bound = int(input())
+upper_bound = int(input())
 
 solution = order_crossover(parent1, parent2, lower_bound, upper_bound)
 
--- python/S1_ Manejo de arrays/solve_tsp.py
+++ python/S1_ Manejo de arrays/solve_tsp.py
@@ -2,19 +2,18 @@ def order_crossover(parent1, parent2, lower_bound, upper_bound):
     child1 = [0] * len(parent1)
     child1[lower_bound:upper_bound] = parent1[lower_bound:upper_bound]
 
-    puntero_hijo = upper_bound
-    puntero_madre = upper_bound
-
-    while puntero_hijo != lower_bound:
-        if puntero_madre == len(parent2):
-            puntero_madre = 0
-        if puntero_hijo == len(child1):
-            puntero_hijo = 0
-        if parent2[puntero_madre] not in parent1[lower_bound:upper_bound]:
-            child1[puntero_hijo] = parent2[puntero_madre]
-            puntero_madre += 1
-            puntero_hijo += 1
+    lugar_child = upper_bound
+    lugar_madre = upper_bound
+    while lugar_child != lower_bound:
+        if lugar_madre == len(parent2):
+            lugar_madre = 0
+        if lugar_child == len(child1):
+            lugar_child = 0
+        if parent2[lugar_madre] not in parent1[lower_bound:upper_bound]:
+            child1[lugar_child] = parent2[lugar_madre]
+            lugar_child += 1
+            lugar_madre += 1
         else:
-            puntero_madre += 1
+            lugar_madre += 1
 
     return child1
