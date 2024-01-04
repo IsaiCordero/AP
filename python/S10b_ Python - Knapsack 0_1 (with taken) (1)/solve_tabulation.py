0dca43574cd039deb642e3e0fd154bcf3b2aa924
Isai Cordero Garcia <125867115+IsaiCordero@users.noreply.github.com>
2024-01-04 16:22:22 +0000

    Update solve_tabulation.py

--- python/S10b_ Python - Knapsack 0_1 (with taken) (1)/solve_tabulation.py
+++ python/S10b_ Python - Knapsack 0_1 (with taken) (1)/solve_tabulation.py
@@ -1,34 +1,37 @@
-
-
 import numpy as np
-def solve_tabulation(items, capacity):
-    taken = []
-    table = np.zeros((len(items)+1, capacity+1), dtype=int)
 
+# Función principal para resolver el problema de la mochila mediante tabulación
+def solve_tabulation(items, capacity):
+    taken = []  # Lista para almacenar los índices de elementos seleccionados
+    table = np.zeros((len(items) + 1, capacity + 1), dtype=int)  # Tabla para almacenar resultados intermedios
+
+    # Llena la tabla con los valores óptimos
     def fill_table():
         for i in range(1, len(items) + 1):
             for j in range(capacity + 1):
-                item_index = i - 1
-                item_value = items[item_index].value
-                item_weight = items[item_index].weight
-
-                if item_weight <= j:
-                    table[i][j] = max(table[i-1][j], item_value + table[i-1][j - item_weight])
+                index = i - 1
+                value = items[index].value  # Valor del elemento actual
+                weight = items[index].weight  # Peso del elemento actual
+                if weight <= j:
+                    # Si el peso del elemento es menor o igual a la capacidad actual,
+                    # calcula el máximo entre tomar o no tomar el elemento en la mochila
+                    table[i][j] = max(table[i - 1][j], value + table[i - 1][j - weight])
                 else:
-                    table[i][j] = table[i-1][j]
+                    # Si el peso del elemento es mayor que la capacidad actual,
+                    # simplemente hereda el valor anterior
+                    table[i][j] = table[i - 1][j]
 
+    # Reconstruye los elementos seleccionados para la mochila óptima
     def fill_taken():
         i = len(items)
         j = capacity
-        cont = 0
         while i > 0 and j > 0:
-            if table[i][j] != table[i-1][j]:
-                taken.insert(0, i)
-                j -= items[i-1].weight
+            if table[i][j] != table[i - 1][j]:
+                taken.insert(0, i)  # Inserta el índice del elemento seleccionado
+                j -= items[i - 1].weight  # Resta el peso del elemento seleccionado
             else:
-                cont += 1
-            i -= 1
+                i -= 1
 
-    fill_table()
-    fill_taken()
-    return table[-1][-1], taken
+    fill_table()  # Llena la tabla con los valores óptimos
+    fill_taken()  # Reconstruye los elementos seleccionados
+    return table[-1][-1], taken  # Devuelve el valor máximo y la lista de elementos seleccionados
