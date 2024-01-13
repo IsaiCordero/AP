9bf0939004199696b6afd99fed35b009dbd6f4ca
Isai Cordero Garcia <125867115+IsaiCordero@users.noreply.github.com>
2024-01-13 12:41:45 +0000

    Update solve_memoization.py

--- python/S09b_ Python - House Robber (with taken) (1)/solve_memoization.py
+++ python/S09b_ Python - House Robber (with taken) (1)/solve_memoization.py
@@ -7,7 +7,7 @@ def solve_memoization(items):
             return 0
         if n in mem:
             return mem[n]
-
+        #crea la tabla con los valores maximos desde n posicion
         mem[n] = max(t(n - 2) + items[n], t(n - 1))
         return mem[n]
 
@@ -18,8 +18,10 @@ def solve_memoization(items):
             if i == 0:
                 taken.append(i)
                 break
+            #si el valor de la tabla actual es el mismo que el siguiente significará que el actual no lo pilla
             if mem[i] == mem[i - 1]:
                 i -= 1
+            #si el valor actual es mayor que el siguiente, entonces significará que robó esa casa
             else:
                 taken.append(i)
                 i -= 2
